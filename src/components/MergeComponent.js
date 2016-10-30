2ab977accdff1858577612d694eaa6f090fbed57
hegelstad <nikolai@hegelstad.net>
2016-10-30 11:31:26 +0100

    minor change

---
+++ src/components/MergeComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const MergeComponent = ({cursor}) => {
+    if (cursor == null) { // Show a placeholder if the cursor of treebeard is not set
+        return (
+            <div className="welcome">
+                placeholder
+            </div>
+        );
+    } else {
+        return (
+            <div className="welcome">
+                <p>name: {cursor.name}</p>
+                <p>id: {cursor.id}</p>
+            </div>
+        );
+    }
+}
+
+export default MergeComponent;
