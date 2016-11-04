0772cb948e8f32d9f435a9c687588b2ad4da9c2f
hegelstad <nikolai@hegelstad.net>
2016-11-04 12:44:44 +0100

    added react-table

--- src/components/WelcomeComponent.js
+++ src/components/WelcomeComponent.js
@@ -1,6 +1,39 @@
 import React, { Component, PropTypes } from 'react';
+import ReactTable from 'react-table'
 
-export default class WelcomeComponent extends Component {
+const data = [{
+    name: 'Tanner Linsley',
+    age: 26,
+    friend: {
+        name: 'Jason Maurer',
+        age: 23
+    }
+},{
+    name: 'Banner Dansley',
+    age: 19,
+    friend: {
+        name: 'Jason Mraz',
+        age: 30
+    }
+}]
+
+const columns = [{
+  header: 'Name',
+  accessor: 'name' // String-based value accessors !
+}, {
+  header: 'Age',
+  accessor: 'age',
+  // render: props => <span className='number'>props.value</span> // Custom cell components!
+}, {
+  header: 'Friend Name',
+  accessor: 'age' // Custom value accessors!
+}, {
+  header: 'Friend Age',
+  //header: props => <span>Friend Age</span>, // Custom header components!
+  accessor: 'friend.age'
+}]
+
+class WelcomeComponent extends Component {
     constructor(...args) {
         super(...args);
 
@@ -18,7 +51,11 @@ export default class WelcomeComponent extends Component {
             <div className="welcome">
                 <p>Select a region, chiefdom or clinic on the left hand menu to start.</p>
                 <p>You will then be given possible duplicates within your selection.</p>
-
+                <ReactTable
+                    data={data}
+                    columns={columns}
+                    minRows={0}
+                />
                 <button id="clear" onClick={this.onClearClick}>Continue</button>
             </div>
         );
@@ -28,3 +65,5 @@ export default class WelcomeComponent extends Component {
 WelcomeComponent.propTypes = {
     onClear: PropTypes.func.isRequired
 };
+
+export default WelcomeComponent;
