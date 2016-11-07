b18b7f908cd8b8bbac0e45924e432d86f4e9539a
hegelstad <nikolai@hegelstad.net>
2016-11-07 17:11:41 +0100

    fixed table

--- src/components/TEIComponent.js
+++ src/components/TEIComponent.js
@@ -1,94 +1,60 @@
 import React, { Component, PropTypes } from 'react';
 import ReactTable from 'react-table';
-
-const data = [{
-    name: '0 Tanner Linsley',
-    age: 26,
-    checked: false,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23
-    }
-},{
-    name: '1 Banner Dansley',
-    age: 19,
-    checked: false,
-    friend: {
-        name: 'Jason Mraz',
-        age: 30
-    }
-},{
-    name: '2 Nikolai Hegelstad',
-    age: 22,
-    checked: false,
-    friend: {
-        name: 'Eirik Berg Nordheim',
-        age: 25
-    }
-},{
-    name: '3 Brad Pitt',
-    age: 40,
-    checked: true,
-    friend: {
-        name: 'Jason Mraz',
-        age: 30
-    }
-},{
-    name: '4 Nikolai Hegelstad',
-    age: 22,
-    checked: false,
-    friend: {
-        name: 'Eirik Berg Nordheim',
-        age: 25
-    }
-},{
-    name: '5 Brad Pitt',
-    age: 40,
-    checked: true,
-    friend: {
-        name: 'Jason Mraz',
-        age: 30
-    }
-}]
-
-const columns = [{
-  header: 'Name',
-  accessor: 'name' // String-based value accessors !
-}, {
-  header: 'Age',
-  accessor: 'age',
-  render: props =>
-        <span>
-            {`${props.value} ${data[props.index].checked} `}
-            <input
-                type="checkbox"
-                defaultChecked={data[props.index].checked}
-                onChange={() => {
-                    //data[props.index] = !props.row.checked;
-                    console.log(props);
-                }}
-            />
-        </span> // Custom cell components!
-}, {
-  header: 'Friend Name',
-  accessor: 'friend.name' // Custom value accessors!
-}, {
-  header: 'Friend Age',
-  //header: props => <span>Friend Age</span>, // Custom header components!
-  accessor: 'friend.age'
-}]
+import { mockdata } from './TEIComponent.mockdata';
 
 class TEIComponent extends Component {
     constructor(...args) {
         super(...args);
 
+        const columns = [
+                {
+                    header: 'Name',
+                    accessor: 'name' // String-based value accessors !
+                },
+                {
+                    header: 'Age',
+                    accessor: 'age',
+                    render: props =>
+                        <span>
+                            {`${props.value} ${this.state.data[props.index].checked} `}
+                            <input
+                                type="checkbox"
+                                checked={this.state.data[props.index].checked}
+                                onChange={() => { // Binds function to this
+                                    let element = this.state.data.slice(props.index, props.index+1);
+                                    element[0].checked = !props.row.checked;
+                                    this.setState({
+                                        data: [
+                                            ...this.state.data.slice(0, props.index),
+                                            ...element,
+                                            ...this.state.data.slice(props.index + 1)
+                                        ]
+                                    })
+                                }}
+                            />
+                        </span> // Custom cell components!
+                },
+                {
+                    header: 'Friend Name',
+                    accessor: 'friend.name'
+                },
+                {
+                    header: 'Friend Age',
+                    accessor: 'friend.age'
+                }
+            ]
+
+        this.state = {
+            data: mockdata,
+            columns: columns
+        };
     }
 
     render() {
         return (
             <ReactTable
-                data={data}
-                columns={columns}
+                data={this.state.data}
+                columns={this.state.columns}
                 minRows={5}
                 pageSize={5}
                 pageSizeOptions={[5, 10]} // The available page size options
---
+++ src/components/TEIComponent.mockdata.js
@@ -0,0 +1,49 @@
+export const mockdata = [{
+    name: '0 Tanner Linsley',
+    age: 26,
+    checked: false,
+    friend: {
+        name: 'Jason Maurer',
+        age: 23
+    }
+},{
+    name: '1 Banner Dansley',
+    age: 19,
+    checked: false,
+    friend: {
+        name: 'Jason Mraz',
+        age: 30
+    }
+},{
+    name: '2 Nikolai Dude',
+    age: 22,
+    checked: false,
+    friend: {
+        name: 'Erik Nordheim',
+        age: 25
+    }
+},{
+    name: '3 Brad Pitt',
+    age: 40,
+    checked: true,
+    friend: {
+        name: 'Jason Mraz',
+        age: 30
+    }
+},{
+    name: '4 Nikolai Hegelstad',
+    age: 22,
+    checked: false,
+    friend: {
+        name: 'Eirik Berg Nordheim',
+        age: 25
+    }
+},{
+    name: '5 Bad Spit',
+    age: 42,
+    checked: true,
+    friend: {
+        name: 'Jason Mraz',
+        age: 34
+    }
+}];
