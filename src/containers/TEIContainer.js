4109c9bb2aa2d6af21030ea4bf0d77da2f2fc2eb
hegelstad <nikolai@hegelstad.net>
2016-11-29 20:14:49 +0100

    small edit

--- src/containers/TEIContainer.js
+++ src/containers/TEIContainer.js
@@ -27,49 +27,49 @@ class TEIContainer extends Component {
         this.state.showAccordion = true;
         this.state.deepSearch = false;
     }
-    
+
     deepSearch() {
         this.state.showAccordion = true;
         this.state.deepSearch = true;
     }
 
     render() {
-        
-
         if (this.props.TeiData.length == 0 || !this.props.cursor) {
             return <div className="loading">Please select a chiefdom or clinic in the list to the left to begin.</div>;
-        } else{
-        return (
-            <div>
-                <h3> Current region: {this.props.cursor.name} </h3>
-                <Button  bsStyle="warning" onClick={this.showAccordion}> Basic Search </Button>
-                <Button bsStyle="danger" onClick={this.deepSearch}> Deep Search</Button>
-                <br />
-                {this.state.showAccordion ?
-                    <div>
-                        <br />
-                        <AccordionList input={teiDuplicateFinder(this.props.TeiData, this.state.deepSearch)}
-                                        columns={Columns}/>
-                    </div> :
-                    <div>
-                        <br /> 
-                        <p> Basic search: matches first and last names with a moderate threshold. </p>
-                        <p> Deep search: will search within the matches of a basic search for matches of more advanced attributes: National Identifier, 
-                            TB number and Maiden name.</p>
-                        <p> Deep search is highly likely to find true duplicates</p>
-                    </div>}
-                
-                <br/>
-            </div>
-        );
+        } else {
+            return (
+                <div>
+                    <h3> Current region: {this.props.cursor.name} </h3>
+                    <Button bsStyle="warning" onClick={this.showAccordion} className="datepicker"> Basic Search</Button>
+                    <Button bsStyle="danger" onClick={this.deepSearch}>Deep Search</Button>
+                    <br />
+                    { this.state.showAccordion
+                        ?
+                        <div>
+                            <br />
+                            <AccordionList
+                                input={teiDuplicateFinder(this.props.TeiData, this.state.deepSearch)}
+                                columns={Columns}
+                            />
+                        </div>
+                        :
+                        <div>
+                            <br />
+                            <p> Basic search: matches first and last names with a moderate threshold. </p>
+                            <p> Deep search: will search within the matches of a basic search for matches of more advanced attributes: National Identifier,
+                                TB number and Maiden name.</p>
+                            <p> Deep search is highly likely to find true duplicates</p>
+                        </div>
+                    }
+                    <br/>
+                </div>
+            );
         }
-
     }
 }
 
 // Shorthand notation.
 export default connect(
-       
     state => ({
         cursor: state.tree.cursor,
         TeiData: state.tei
